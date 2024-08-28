1f1da647df424013d60e4fbdd38e1176804d8886
tpk-Deploramous <mikebigby@gmail.com>
2024-08-27 21:24:56 -0700

    Updated main and added main-test

--- main.py
+++ main.py
@@ -1,42 +1,70 @@
-from string import ascii_lowercase
+fireball_damage = 500
+potion_mana = 100
+fireball_cost = 50
+
+
+class Wizard:
+    def __init__(self, name, stamina, intelligence):
+        self.name = name
+        self.__stamina = stamina
+        self.__intelligence = intelligence
+        self.mana = self.__intelligence * 10
+        self.health = self.__stamina * 100
+
+    def cast_fireball(self, target):
+        if self.mana >= 50:
+            self.mana -= fireball_cost
+            target.get_fireballed()
+        else:
+            raise Exception(f"{self.name} cannot cast fireball")
+
+    def __is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+
+    def get_fireballed(self):
+        self.health -= fireball_damage
+
+    def drink_mana_potion(self):
+        self.mana += potion_mana
+
+
+# don't touch below this line
+
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    word_count = get_word_count(text)
-    char_counts = get_letters_counts(text)
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"There are {word_count} words in this book.\n\n")
-    report_formatting(char_counts)
-    print("--- END REPORT ---")
+    merlin = Wizard("Merlin", 15, 10)
+    morgana = Wizard("Morgana", 17, 5)
+    print_wizard_stats(merlin)
+    print_wizard_stats(morgana)
+
+    while merlin._Wizard__is_alive() and morgana._Wizard__is_alive():
+        test_cast(merlin, morgana)
+        test_cast(morgana, merlin)
+
+    print(" -- Done! --")
 
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+def test_cast(caster, target):
+    print(f"  >  {caster.name} casts fireball at {target.name}")
+    try:
+        caster.cast_fireball(target)
+    except Exception as e:
+        print(f"    >  !!!{e}!!!")
+        test_drink_potion(caster)
+    print_wizard_stats(caster)
+    print_wizard_stats(target)
 
 
-def get_word_count(text):
-    text_split = text.split()
-    words = len(text_split)
-    return words
+def test_drink_potion(caster):
+    print(f"  >  {caster.name} drinks mana potion")
+    caster.drink_mana_potion()
 
-def get_letters_counts(text):
-    alpha_dict = {}
-    for letter in text:
-        lower = letter.lower()
-        if alpha_dict.get(lower) == None:
-            alpha_dict[lower] = 1
-        else:
-            alpha_dict[lower] = alpha_dict.get(lower) + 1
-    return alpha_dict
 
-def report_formatting(char_counts_raw):
-    sorted_dict = dict(sorted(char_counts_raw.items(), reverse=True, key=lambda i: i[1]))
-    for key in sorted_dict:
-        if key.isalpha() == True:
-            print(f"The letter {key} appears {sorted_dict[key]} times.")
-
+def print_wizard_stats(wizard):
+    print(f"{wizard.name}: health={wizard.health}, mana={wizard.mana}")
 
 
 main()
